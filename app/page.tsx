b3d5282ad70214e3522a38bc7f7c453c32c5ea39
Joaquín <joaquingomezleites@gmail.com>
2024-03-30 20:47:12 -0300

    feat: collaborator card & svg fiddling around

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
+import { ArrowRight, LinkedinIcon } from "lucide-react";
 
 import { buttonVariants } from "@/components/ui/button";
 import ContactButton from "@/components/ui/contact-button";
 import Heading from "@/components/ui/heading";
 import { cn } from "@/lib/utils";
-import { ArrowRight, ArrowUp } from "lucide-react";
-import Image from "next/image";
 import ProjectLink from "@/components/ui/project-link";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Collaborator from "@/components/ui/collaborator";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -33,17 +35,57 @@ export default function Home() {
         </div>
 
         {/* Most recent project */}
+        <ProjectLink
+          href="/"
+          title="Nuestro más reciente trabajo"
+          description="XLVI International Congress of Theoretical Chemists of Latin Expression"
+          imageUrl="/quitel.png"
+          imageAlt="XLVI International Congress of Theoretical Chemists of Latin Expression"
+        />
 
-        <div className="">
-          <ProjectLink
-            href="/"
-            title="Nuestro más reciente trabajo"
-            description="XLVI International Congress of Theoretical Chemists of Latin Expression"
-            imageUrl="/quitel.png"
-            imageAlt="XLVI International Congress of Theoretical Chemists of Latin Expression"
+        {/* Profiles */}
+        <div className="relative flex flex-col items-center gap-y-8">
+          <Image
+            src={'/planet.svg'}
+            alt="bandicoot planet"
+            width={100}
+            height={100}
+            className="absolute -right-2 top-4 -z-10 w-24 lg:w-32 lg:-right-6 lg:top-0 "
           />
-
+          <Image
+            src={'/rocket-svgrepo.svg'}
+            alt="rocket-svgrepo"
+            width={100}
+            height={100}
+            className="absolute w-12 left-0 -bottom-6 rotate-12"
+          />
+          <h1 className="font-bold text-4xl border-b-2 border-orange-500">
+            Bandicoot
+          </h1>
+          <div className="flex gap-x-2 w-full">
+            <Collaborator
+              name="Agustín"
+              role="Fundador"
+              avatarUrl="https://media.licdn.com/dms/image/D4D35AQFIZ0_TKnX4xg/profile-framedphoto-shrink_800_800/0/1689806914785?e=1712440800&v=beta&t=Awdmsx288l8EkMEfnitT3VE2ywwCcgpSB2xagZfp5UQ"
+              avatarAlt="@agustin-lasalvia"
+              avatarFallback="AS"
+              linkedinUrl="https://www.linkedin.com/in/agustin-lasalvia/"
+            />
+            <Collaborator
+              name="Joaquín"
+              role="Fundador"
+              avatarUrl="https://media.licdn.com/dms/image/C4D03AQFDd2VKmtq0fw/profile-displayphoto-shrink_800_800/0/1625511672026?e=1717027200&v=beta&t=8V3K7NX-HRhjui9-vLBmDEziZdDH0WHtoFcCgGqGgkM"
+              avatarAlt="@joaquingomezleites"
+              avatarFallback="JG"
+              linkedinUrl="https://www.linkedin.com/in/joaquingomezleites/"
+            />
+          </div>
         </div>
+
+        <footer className="">
+
+        </footer>
+
       </div>
     </main>
   );
---
+++ components/ui/collaborator.tsx
@@ -0,0 +1,44 @@
+import { LinkedinIcon } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "./button";
+
+const Collaborator = ({
+    name,
+    role,
+    avatarUrl,
+    avatarAlt,
+    avatarFallback,
+    linkedinUrl,
+}: {
+    name: string;
+    role: string;
+    avatarUrl: string;
+    avatarAlt: string;
+    avatarFallback: React.ReactNode;
+    linkedinUrl: string;
+}) => {
+    return (
+        <div className="flex flex-col items-center py-4 px-4 bg-background shadow-lg shadow-orange-500/20 rounded-xl w-full">
+            <Avatar className="w-24 h-24">
+                <AvatarImage
+                    src={avatarUrl}
+                    alt={avatarAlt}
+                />
+                <AvatarFallback>{avatarFallback}</AvatarFallback>
+            </Avatar>
+            <p className="text-lg font-bold">{name}</p>
+            <p className="text-sm text-muted-foreground">{role}</p>
+            <Link
+                href={linkedinUrl}
+                className={cn((buttonVariants({ variant: 'ghost', size: 'icon' })), "my-2")}
+                target="_blank"
+            >
+                <LinkedinIcon className="h-6 w-6" />
+            </Link>
+        </div>
+    );
+}
+
+export default Collaborator;
