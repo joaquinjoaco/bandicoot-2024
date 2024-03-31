206f17c32f383ee1b6b56567cc12b39c98df14f7
Joaquín <joaquingomezleites@gmail.com>
2024-03-31 19:45:28 -0300

    changed contact email

--- components/ui/contact-button.tsx
+++ components/ui/contact-button.tsx
@@ -11,7 +11,7 @@ const ContactButton = ({
 }) => {
     return (
         <a
-            href='mailto:aguslblumenfeld@gmail.com'
+            href='mailto:bandicoot.solutions@gmail.com'
             className={cn((buttonVariants({ variant: 'default' })), "group text-lg font-normal lg:font-medium lg:text-sm lg:hover:font-bold bg-orange-500 hover:bg-orange-500/90 transition-all", className)}
         >
             <Mail className="h-4 w-4 mr-2 lg:group-hover:w-0 lg:group-hover:mr-0 transition-all" />
